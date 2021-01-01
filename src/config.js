64cd1bda50c1801cf29a1efb1eedb04b8ef9ce7e
sam p <spoveda545@gmail.com>
2021-01-01 03:23:38 -0500

    modified config to be consitent with heroku env vars and added test db reference

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://USERNAME@localhost/DATABASE_NAME',
+  DATABASE_URL: process.env.DB_URL || 'postgresql://postgres@localhost/yamma',
+	TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://postgres@localhost/yamma_test'
   JWT_SECRET: process.env.JWT_SECRET || 'yamma-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };
