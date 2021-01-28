cca4016581ca9da419e886a64534f7447ba91b7e
Noah <aminnnoah19@gmail.com>
2021-01-27 19:41:27 -0600

    db and testdb url

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/yamma',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/yammadb',
   TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/yamma_test',
+    process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/yammatestdb',
   JWT_SECRET: process.env.JWT_SECRET || 'yamma-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };
