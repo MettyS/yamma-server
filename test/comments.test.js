efee32cd76d7a9e7ee6f36af10e576d8c9b5b21b
Metty Schroeder <mettyj.schroeder@gmail.com>
2021-01-28 12:55:23 -0500

    updated test_database variable name in tests

--- test/comments.test.js
+++ test/comments.test.js
@@ -24,7 +24,7 @@ describe('comments endpoints', function () {
   before('make knex instance', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     });
     app.set('db', db);
   });
